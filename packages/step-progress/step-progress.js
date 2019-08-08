8622d9a2a635f760f572ce8aa5c8a9a6bb50c171
jobizzness <jobizzness@gmail.com>
2019-08-08 12:44:41 +0800

    step progress will position nodes

--- packages/step-progress/step-progress.js
+++ packages/step-progress/step-progress.js
@@ -16,6 +16,9 @@ class StepProgress extends LitElement {
       },
       prop1: {
         type: String
+      },
+      active: {
+        type: Number
       }
     };
   }
@@ -27,28 +30,53 @@ class StepProgress extends LitElement {
         position: relative;
       }
       .wrapper {
-        height: 30px;
+        height: 50px;
         width: 100%;
         position: relative;
+        overflow: hidden;
       }
       biness-step-progress-bar {
         background-color: #bde1ec6b;
         height: 5px;
         position: absolute;
         width: 100%;
+        transform: 0.4s linear;
         top: 19px;
       }
-      biness-step-progress-node {
+      biness-step-progress-bar::after {
+        content: '';
         position: absolute;
+        left: 0;
+        transform: translateX(-50%);
+        width: 100%;
+        height: 4px;
+        background-color: #0bb1e4;
+      }
+      biness-step-progress-node {
+        cursor: pointer;
+        position: absolute;
+        background: white;
         left: 0;
         transform: translateX(0%);
         border-radius: 50%;
-        padding: 8px 14px;
+        padding: 8px 13px;
+        border: 4px solid #bde1ec;
+        color: #0bb1e4;
+      }
+      biness-step-progress-node[status='active'] {
+        border-color: #0bb1e4;
+      }
+      biness-step-progress-node[status='done'] {
         background: #0bb1e4;
-        border: 7px solid #bde1ec;
         color: white;
       }
     `;
+  }
+
+  constructor() {
+    super();
+    this.nodes$ = [];
+    this.bar$ = null;
   }
 
   render() {
@@ -67,6 +95,36 @@ class StepProgress extends LitElement {
 
   firstUpdated() {
     this.prop1 = 'Step progress';
+    this.init();
+  }
+
+  setActiveNode() {}
+
+  init() {
+    this.nodes$ = this.shadowRoot.querySelectorAll('biness-step-progress-node');
+    this.wrapper$ = this.shadowRoot.querySelector('.wrapper');
+    const numberOfSteps = this.nodes$.length;
+    const wrapperWidth = this.wrapper$.offsetWidth - 43;
+    const offset = wrapperWidth / numberOfSteps;
+
+    // Of all our nodes
+    this.nodes$.forEach((node, index) => {
+      let translate;
+
+      // Determine where to position this node
+      if (this._isLastNode(index, numberOfSteps)) {
+        translate = wrapperWidth;
+      } else {
+        translate = offset * 1.4 * index;
+      }
+
+      // Set the style for this node
+      node.setAttribute('style', `transform: translateX(${translate}px)`);
+    });
+  }
+
+  _isLastNode(node, numberOfSteps) {
+    return node === numberOfSteps - 1;
   }
 }
 
